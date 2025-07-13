bd9f91c9fcb36aea2c7c22d78e2a466b3142922d
Benjamin Lehmann <bclehmann@yahoo.ca>
2025-07-12 20:26:17 -0600

    Optimizations

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ pushd assembly-script
 npm run asbuild
 popd
 
-emcc ./emscripten/main.c -o ./emscripten/a.out.js -sEXPORTED_FUNCTIONS=_naiveMatrixMultiplication,_malloc,_free -sEXPORTED_RUNTIME_METHODS=ccall,HEAPF32
+emcc ./emscripten/main.c -o ./emscripten/a.out.js -sEXPORTED_FUNCTIONS=_naiveMatrixMultiplication,_malloc,_free -sEXPORTED_RUNTIME_METHODS=ccall,HEAPF32 -O3
