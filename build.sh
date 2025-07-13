0f3b3e30fe0de869ea2caab67992b60281354141
Benjamin Lehmann <bclehmann@yahoo.ca>
2025-07-12 20:14:43 -0600

    Add build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+pushd assembly-script
+npm run asbuild
+popd
+
+emcc ./emscripten/main.c -o ./emscripten/a.out.js -sEXPORTED_FUNCTIONS=_naiveMatrixMultiplication,_malloc,_free -sEXPORTED_RUNTIME_METHODS=ccall,HEAPF32
