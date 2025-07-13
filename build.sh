9fb5588aac7e38efe7e5e6be3d372e2835ffd8c8
Benjamin Lehmann <bclehmann@yahoo.ca>
2025-07-12 22:53:34 -0600

    My favourite compiler flags

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ pushd assembly-script
 npm run asbuild
 popd
 
-emcc ./emscripten/main.c -o ./emscripten/a.out.js -sEXPORTED_FUNCTIONS=_naiveMatrixMultiplication,_malloc,_free -sEXPORTED_RUNTIME_METHODS=ccall,HEAPF32 -O3
+emcc ./emscripten/main.c -o ./emscripten/a.out.js -sEXPORTED_FUNCTIONS=_naiveMatrixMultiplication,_malloc,_free -sEXPORTED_RUNTIME_METHODS=ccall,HEAPF32 -O3 -Wall -Wextra -Wpedantic
 
 pushd ffi/build
 make
